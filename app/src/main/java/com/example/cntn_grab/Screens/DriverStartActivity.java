58c2c91bc88fb2884dc73d3c2cf1b56e60b84af9
khactrieu99 <khactrieu.personal@gmail.com>
2019-12-26 13:24:49 +0700

    update ui #3

---
+++ app/src/main/java/com/example/cntn_grab/Screens/DriverStartActivity.java
@@ -0,0 +1,16 @@
+package com.example.cntn_grab.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cntn_grab.R;
+
+public class DriverStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_start);
+    }
+}
