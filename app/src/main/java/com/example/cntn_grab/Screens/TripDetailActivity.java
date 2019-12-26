b4580bf2f8eee935660d5e35869eb08153378ea7
khactrieu99 <khactrieu.personal@gmail.com>
2019-12-26 20:47:40 +0700

    update ui #5

---
+++ app/src/main/java/com/example/cntn_grab/Screens/TripDetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.cntn_grab.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cntn_grab.R;
+
+public class TripDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_detail);
+    }
+
+
+}
