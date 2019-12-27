67867f7e0c16922968e35d74f62dd159b924d45a
khactrieu99 <khactrieu.personal@gmail.com>
2019-12-27 12:42:00 +0700

    update ui #6

--- app/src/main/java/com/example/cntn_grab/Screens/DriverStartFragment.java
+++ app/src/main/java/com/example/cntn_grab/Screens/DriverStartFragment.java
@@ -4,20 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.cntn_grab.R;
 
 public class DriverStartFragment extends Fragment {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    LinearLayout suportLayout;
+    LinearLayout findTripButton;
+
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.activity_driver_start, container, false);
+
+        suportLayout = view.findViewById(R.id.driver_support_area);
+        findTripButton = view.findViewById(R.id.find_trip_button);
+
+        findTripButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                suportLayout.removeAllViews();
+                View child = getLayoutInflater().inflate(R.layout.driver_support_layout, null);
+                suportLayout.addView(child);
+            }
+        });
 
         return view;
     }
---
+++ app/src/main/java/com/example/cntn_grab/Screens/TripInfoFragment.java
@@ -0,0 +1,28 @@
+package com.example.cntn_grab.Screens;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cntn_grab.R;
+
+public class TripInfoFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_trip_info, container, false);
+
+        return view;
+    }
+
+
+}
